549917148da5ddcfd07b87b2ff36954363b2b425
Lonealpha7 <mohdtasnim9@gmail.com>
2024-02-16 15:27:28 +0300

    Initial Commit

---
+++ Employee.py
@@ -0,0 +1,27 @@
+class Employee:
+    def _init_(self, name, age, emp_id, department):
+        self.name = name
+        self.age = age
+        self.emp_id = emp_id
+        self.department = department
+
+class EmployeeManagementSystem:
+    def _init_(self):
+        self.employees = []
+
+    def create_employee(self, name, age, emp_id, department):
+        employee = Employee(name, age, emp_id, department)
+        self.employees.append(employee)
+
+    def get_employee_by_id(self, emp_id):
+        for employee in self.employees:
+            if employee.emp_id == emp_id:
+                return employee
+        return None
+
+    def delete_employee_by_id(self, emp_id):
+        for employee in self.employees:
+            if employee.emp_id == emp_id:
+                self.employees.remove(employee)
+                return True
+        return False
